1c62cc786fcca10988aa617dcf184dce5e5c1147
CutzuDev <alexfarkas05@gmail.com>
2022-10-11 21:04:44 +0300

    Nav Buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div>
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,28 @@
 import "./Home.css";
+import { Link } from "react-router-dom";
+import AppsIcon from "@mui/icons-material/Apps";
+import { Avatar } from "@mui/material";
 
 function Home() {
-  return <div className="home">HOME</div>;
+  return (
+    <div className="home">
+      <div className="home__navbar">
+        <ul className="home__navbar--left home__navbar">
+          <div className="home__navbar--link">
+            <Link to="/about">About</Link>
+            <Link to="/store">Store</Link>
+          </div>
+        </ul>
+        <ul className="home__navbar--right home__navbar">
+          <Link to="/gmail">Gmail</Link>
+          <Link to="/images">Images</Link>
+          <AppsIcon />
+          <Avatar />
+        </ul>
+      </div>
+      <div className="home__body"></div>
+    </div>
+  );
 }
 
 export default Home;
