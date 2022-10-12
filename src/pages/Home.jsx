1006a110727e2f747a7bc55122dd98ab98de17d4
CutzuDev <alexfarkas05@gmail.com>
2022-10-12 17:14:31 +0300

    Styling + image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,25 +2,31 @@ import "./Home.css";
 import { Link } from "react-router-dom";
 import AppsIcon from "@mui/icons-material/Apps";
 import { Avatar } from "@mui/material";
+import GoogleLogo from "../assets/googlelogo.png";
 
 function Home() {
   return (
     <div className="home">
-      <div className="home__navbar">
-        <ul className="home__navbar--left home__navbar">
-          <div className="home__navbar--link">
-            <Link to="/about">About</Link>
-            <Link to="/store">Store</Link>
-          </div>
-        </ul>
-        <ul className="home__navbar--right home__navbar">
-          <Link to="/gmail">Gmail</Link>
-          <Link to="/images">Images</Link>
-          <AppsIcon />
-          <Avatar />
-        </ul>
+      <nav className="home__navbar--container">
+        <div className="home__navbar">
+          <ul className="home__navbar--left home__navbar--item-container">
+            <div className="home__navbar--link">
+              <Link to="/about">About</Link>
+              <Link to="/store">Store</Link>
+            </div>
+          </ul>
+          <ul className="home__navbar--right home__navbar--item-container">
+            <Link to="/gmail">Gmail</Link>
+            <Link to="/images">Images</Link>
+            <AppsIcon className="mui__icons" />
+            <Avatar className="mui__icons" />
+          </ul>
+        </div>
+      </nav>
+
+      <div className="home__body">
+        <img src={GoogleLogo} alt="" />
       </div>
-      <div className="home__body"></div>
     </div>
   );
 }
