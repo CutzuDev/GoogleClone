af10aced6b9f761dd52f92e6271444f93cea87c2
CutzuDev <alexfarkas05@gmail.com>
2022-10-14 21:48:08 +0300

    Update

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,34 +3,67 @@ import SearchIcon from "@mui/icons-material/Search";
 import MicIcon from "@mui/icons-material/Mic";
 import { Button } from "@mui/material";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-function Search() {
+function Search({ hideButtons = false }) {
   const [search, setSearch] = useState("");
+  const navigate = useNavigate();
 
   const searchButton = (e) => {
     e.preventDefault();
+
+    console.log("Search button hit", search);
+
+    navigate("/search");
   };
 
-  useEffect(() => {
-    console.log(search);
-  }, [search]);
+  // useEffect(() => {
+  //   console.log(search);
+  // }, [search]);
 
   return (
-    <div className="search">
+    <form className="search">
       <div className="search__input">
         <SearchIcon className="search__icon" />
-        <input onChange={(e) => setSearch(e.target.value)} />
+        <input value={search} onChange={(e) => setSearch(e.target.value)} />
         <MicIcon className="search__icon" />
       </div>
-      <div className="search__buttons">
-        <Button onClick={searchButton} variant="outlined" color="grey">
-          Google Search
-        </Button>
-        <Button variant="outlined" color="grey">
-          I'm Feeling Lucky
-        </Button>
-      </div>
-    </div>
+
+      {!hideButtons ? (
+        <div className="search__buttons">
+          <Button
+            type="submit"
+            onClick={searchButton}
+            variant="outlined"
+            color="grey"
+          >
+            Google Search
+          </Button>
+          <Button variant="outlined" color="grey">
+            I'm Feeling Lucky
+          </Button>
+        </div>
+      ) : (
+        <div className="search__buttons sb__hidden">
+          <Button
+            type="submit"
+            onClick={searchButton}
+            variant="outlined"
+            color="grey"
+          >
+            Google Search
+          </Button>
+          <Button
+            type="submit"
+            onClick={searchButton}
+            variant="outlined"
+            color="grey"
+          >
+            I'm Feeling Lucky
+          </Button>
+        </div>
+      )}
+    </form>
   );
 }
 
